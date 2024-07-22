c685f0442298d1ad7e094c7fbfa075579f255028
Pardhu-Guttula <drpardhu007@gmail.com>
2024-07-22 22:46:40 +0530

    modified in BE .env

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import caseRoutes from "./interface/routes/CaseRoutes";
 import setupSwagger from "./utils/swagger";
 import dotenv from "dotenv";
